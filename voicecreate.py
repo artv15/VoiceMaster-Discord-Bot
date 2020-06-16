97d460d38e530036a4dac1174afc7eb64f4adbbc
artv15 <35376120+artv15@users.noreply.github.com>
2020-06-16 14:17:10 +0300

    Update voicecreate.py

--- voicecreate.py
+++ voicecreate.py
@@ -13,7 +13,7 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix=".")
 bot.remove_command("help")
-DISCORD_TOKEN = 'Enter Discord Token here'
+DISCORD_TOKEN = BOT_TOKEN
 
 initial_extensions = ['cogs.voice']
 
